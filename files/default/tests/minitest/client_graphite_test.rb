6b9775abdc8c67095ae1d662073c6fb66887e9de
Matt Alexander <mattjalexander@gmail.com>
2013-09-25 14:12:04 -0400

    Fix client_graphite test

--- files/default/tests/minitest/client_graphite_test.rb
+++ files/default/tests/minitest/client_graphite_test.rb
@@ -4,7 +4,13 @@ require File.expand_path('../support/helpers',__FILE__)
 describe 'collectd::client_graphite' do
   include Helpers::Collectd
 
-  it 'creates carbon_writer.conf' do
-    file("#{node['collectd']['plugconf_dir']}/carbon_writer.conf").must_exist
+  it 'creates graphite plugin configuration' do
+    if node['collectd']['version'] =~ /5\.\d+/
+      conf = "write_graphite.conf"
+    else
+      conf = "carbon_writer.conf"
+    end
+
+    file("#{node['collectd']['plugconf_dir']}/#{conf}").must_exist
   end
 end
